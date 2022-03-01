1c0ea0abb80dd1e854da91d174bbe727792276e9
Shih-Ming Wang <swang150@ucsc.edu>
2022-02-28 17:51:57 -0800

    Iteratools separate list.

--- lua/igit/itertools.lua
+++ lua/igit/itertools.lua
@@ -1,19 +1,30 @@
 local M = {}
 
 M.iter = {}
-function M.iter:new(next_fn, invariant, control, map_fn)
-    if type(next_fn) ~= 'function' then
-        invariant = next_fn
-        next_fn = next
-        control = nil
+function M.iter:new(opts)
+    vim.validate({next_fn = {opts.next_fn, {'function', 'table'}}})
+    if type(opts.next_fn) ~= 'function' then
+        opts.invariant = opts.next_fn
+        opts.next_fn = next
+        opts.control = nil
     end
+    opts.map_fn = opts.map_fn or function(e) return e end
+    vim.validate({
+        next_fn = {opts.next_fn, 'function'},
+        invariant = {opts.invariant, 'table', true},
+        control = {opts.controle, 'number', true},
+        map_fn = {opts.map_fn, 'function'},
+        is_list = {opts.is_list, 'boolean', true}
+    })
+
     local obj = {}
     setmetatable(obj, self)
     self.__index = self
-    obj.next = next_fn
-    obj.invariant = invariant
-    obj.control = control
-    obj.map_fn = map_fn or function(e) return e end
+    obj.next = opts.next_fn
+    obj.invariant = opts.invariant
+    obj.control = opts.control
+    obj.map_fn = opts.map_fn
+    obj.is_list = opts.is_list
     return obj
 end
 setmetatable(M.iter, {__call = function(cls, ...) return cls:new(...) end})
@@ -22,19 +33,22 @@ function M.iter:pairs() return self.next, self.invariant, self.control end
 
 function M.iter:collect()
     local res = {}
-    for k, v in self:pairs() do
-        if v == nil then
-            res[#res + 1] = self.map_fn(k)
-        else
-            res[k] = self.map_fn(v)
-        end
+    if self.is_list then
+        for k in self:pairs() do res[#res + 1] = self.map_fn(k) end
+    else
+        for k, v in self:pairs() do res[k] = self.map_fn(v) end
     end
     return res
 end
 
 function M.iter:map(map_fn)
-    return M.iter(self.next, self.invariant, self.control,
-                  function(e) return map_fn(self.map_fn(e)) end)
+    return M.iter({
+        next_fn = self.next,
+        invariant = self.invariant,
+        control = self.control,
+        is_list = self.is_list,
+        map_fn = function(e) return map_fn(self.map_fn(e)) end
+    })
 end
 
 function M.range(a, b, step)
@@ -50,7 +64,7 @@ function M.range(a, b, step)
         local nextvalue = lastvalue + step
         if nextvalue >= b then return nextvalue end
     end or function(_, lastvalue) return lastvalue end
-    return M.iter(f, nil, a - step)
+    return M.iter({next_fn = f, control = a - step, is_list = true})
 end
 
 return M
