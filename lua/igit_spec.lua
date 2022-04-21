e97fbbd73516ba8f2dc8da2ac0ebfea4b43501c9
Shih-Ming Wang <swang150@ucsc.edu>
2022-04-20 23:46:49 -0700

    Add test for modifiers.

--- lua/igit_spec.lua
+++ lua/igit_spec.lua
@@ -1,5 +1,8 @@
 local igit = require("igit")
 local default_config = require("igit.default_config")
+local spy = require("luassert.spy")
+local match = require("luassert.match")
+local log = require("igit.log")
 
 describe("igit setup", function()
 	it("Defaults submodule options to default_config", function()
@@ -21,22 +24,37 @@ describe("igit setup", function()
 	end)
 
 	it("Honors git_sub_commands options", function()
-		local git = require("igit.git")
-		local stub = require("luassert.stub")(git, "with_default_args")
-		stub.by_default.returns(require("igit.git"))
 		local fake_command = "aaa"
+		local git = require("igit.git")
+		local s = spy.on(git, "with_default_args")
 
 		-- Parser fails to parse the fake_command, with_default_args was never
 		-- called to form the git command to run.
 		vim.cmd("IGit " .. fake_command)
-		assert(not stub:called())
+		assert.spy(s).was_not_called()
 
 		-- Parser succeeds, with_default_args was called to form the git command
 		-- to run.
 		igit.setup({ git_sub_commands = { "aaa" } })
 		vim.cmd("IGit " .. fake_command)
-		assert(stub:called())
+		assert.spy(s).was_called()
 
-		stub:revert()
+		s:revert()
 	end)
+end)
+
+describe("IGit command", function()
+	local function test_mod(modifier)
+		it("Honors modifier " .. modifier, function()
+			local s = spy.on(igit.log, "open")
+			vim.cmd(modifier .. " IGit log")
+			local _ = match._
+			assert.spy(s).was_called_with(_, _, modifier .. " split")
+			s:revert()
+		end)
+	end
+	test_mod("belowright")
+	test_mod("topleft")
+	test_mod("aboveleft")
+	test_mod("vertical")
 end)
